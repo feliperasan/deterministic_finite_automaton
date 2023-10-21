b55a01eca24ceda4fa4288ddf1c6c56be1ed60ef
Felipe Barbosa <rafaelt.ibarbosa@gmail.com>
2023-10-21 20:45:02 -0300

    Correcoes

--- src/App.java
+++ src/App.java
@@ -1,16 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 import util.AFD;
 
 class App {
-
-    
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        String entrada = "10";
+        System.out.println("Digite a sentença: ");
+        String entrada = sc.nextLine();
         
         if(AFD.processar(entrada)) {
-            System.out.println("A cadeia é valida!");
+            System.out.println("\u001B[32m\u001B[1mCadeia Aceita!");
+            System.out.println("");
         } else {
-            System.out.println("A cadeia não é valida!");
+            System.out.println("\u001B[31m\u001B[1mCadeia Rejeitada!");
+            System.out.println("");
         }
+
+        sc.close();
     }
 }
